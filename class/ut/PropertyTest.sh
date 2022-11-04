5c1336fb138db50aa14165c200041e8ff2dbdf40
Shigeo NISHIDA <shigeo.nsd@gmail.com>
2022-11-04 09:08:21 +0900

    プロパティテスト用クラス追加。

---
+++ class/ut/PropertyTest.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+# PropertyTest.sh -- PropertyTest クラス
+#
+#
+
+# Constructor
+function PropertyTest() {
+    local ___super=null;
+    local ___class=${FUNCNAME};
+    local ___this="$1";
+    shift;
+
+    public    string   name     = "Shigeo NISHIDA";
+    public    any      addr     = "Kochi, Japan ";
+    protected int      age      = "51";
+    private   date     birthday = "1971/02/15";
+    private   datetime now      = "$(date '+%Y/%m/%d %T')";
+
+    _new "$@";
+}
