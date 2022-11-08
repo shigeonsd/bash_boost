4e9e82002f3fbac5a299015f99254b0c3f1dcd57
Shigeo NISHIDA <shigeo.nsd@gmail.com>
2022-11-08 21:19:49 +0900

    array 関数単体テスト実装中。

--- funcs/array.bash
+++ funcs/array.bash
@@ -3,8 +3,8 @@
 # array.sh -- 配列・連想配列用関数
 #
 function array_exists() {
-    local val="$1";
-    local array="$2";
+    local val="${1}";
+    declare -n array="${2}";
     local v;
     for v in "${array[@]}"; do
 	[ "${v}" == "${val}" ] && { return 0; }
@@ -13,8 +13,8 @@ function array_exists() {
 }
 
 function array_key_exists() {
-    local key="$1";
-    local array="$2";
+    local key="${1}";
+    declare -n array="${2}";
     local k;
     for k in "${!array[@]}"; do
 	[ "${k}" == "${array[${key}]}" ] && { return 0; }
