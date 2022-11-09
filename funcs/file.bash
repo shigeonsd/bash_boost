2240a14f80d4b39f8766b44b87c637fbb1a8842e
Shigeo NISHIDA <shige.nsd@gmail.com>
2022-11-09 16:18:06 +0900

    file.ut を実装。

--- funcs/file.bash
+++ funcs/file.bash
@@ -5,14 +5,17 @@
 function create_fd() {
     local file="${1}";
     local fd=0;
-    touch "${file}";
+    touch "${file}" || return 0;
     exec  {fd}>"${file}";
     return ${fd};
 }
 
 function create_file() {
     local file="${1}";
-    touch "${file}";
+    touch "${file}" || {
+	echo "";
+	return 1;
+    }
     echo ${file};
 }
 
