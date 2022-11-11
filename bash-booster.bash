dd2803e90837d226a692ba7bcc0fe79cd9400f39
Shigeo NISHIDA <shige.nsd@gmail.com>
2022-11-11 14:49:53 +0900

    ファイルの source 方法を改善。

--- bash-booster.bash
+++ bash-booster.bash
@@ -19,22 +19,5 @@ extra_funcs_dir="${funcs_dir}/extra";
 hostname=$(hostname);
 _hostname=$(hostname | sed -e 's/-/_/g');
 
-source \
-    ${funcs_dir}/array.sh    \
-    ${funcs_dir}/date.sh     \
-    ${funcs_dir}/debug.sh    \
-    ${funcs_dir}/defun.sh    \
-    ${funcs_dir}/exists.sh   \
-    ${funcs_dir}/file.sh     \
-    ${funcs_dir}/if.sh       \
-    ${funcs_dir}/log.sh      \
-    ${funcs_dir}/misc.sh     \
-    ${funcs_dir}/required.sh \
-    ;
-
-source \
-    ${extra_funcs_dir}/usage.sh \
-    ${extra_funcs_dir}/load.sh  \
-    ${extra_funcs_dir}/class.sh \
-    ${extra_funcs_dir}/aop.sh   \
-    ;
+source $(${funcs_dir}/FILES);
+source $(${extra_funcs_dir}/FILES);
